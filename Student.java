612b23eb015d66e8c4c43ec4436d840aca29b8aa
AssiaGouti <162887598+AssiaGouti@users.noreply.github.com>
2025-12-09 22:59:22 +0100

    Implement Student class with properties and methods

--- Student.java
+++ Student.java
@@ -1,1 +1,37 @@
+package app.models;
 
+public class Student {
+  
+    private int id;
+    private String name;
+    private String module;
+    private double note;
+
+
+    public Student(Int id, String name, String module, double note) {
+        this.id = id;
+        this.name = name;
+        this.module = module;
+        this.note = note;
+    }
+    public int getId(){
+      return id;
+    }
+  
+    public String getName() {
+        return name;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + module + " | " + note;
+    }
+}
